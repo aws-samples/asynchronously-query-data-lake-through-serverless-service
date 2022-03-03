b76f7fc26dd52101a26fd950031039fd50f035da
weichil <weichil@amazon.com>
2022-03-03 15:41:12 +0000

    Add Rest API Gateway to Stepfunction

--- cdk/infrastructure/stacks.py
+++ cdk/infrastructure/stacks.py
@@ -5,7 +5,7 @@ from aws_cdk import (
     Duration,
     aws_stepfunctions as _sf,
     aws_stepfunctions_tasks as _tasks,
-    aws_apigatewayv2 as _apigwv2,
+    aws_apigateway as _apigw,
     aws_lambda as _lambda,
     aws_iam as _iam,
 )
@@ -102,66 +102,54 @@ class AsyncQueryAthenaStack(Stack):
         )
 
         self._api_role = self._create_apigw_to_stepfunction_start_exec_role()
-        self._http_api = self._create_apigw_v2()
-        self._stage = self._create_stage()
-        self._route = self._create_stepfunction_start_exec_route()
     
+        # Create REST API Gateway
+        self._rest_api = _apigw.RestApi(
+            self,'AsyncAthenaQueryRestApi',
+            rest_api_name='AsyncAthenaQuery'
+        )
+
+        # Create invocation resources
+        self._invocation_res = self._rest_api.root.add_resource('invocation')
+
+        # StepFunction integration
+        self._stepfunction_integ = _apigw.AwsIntegration(
+            service='states',
+            action='StartExecution',
+            options=_apigw.IntegrationOptions(
+                credentials_role=self._api_role,
+                request_templates={
+                    'application/json':
+                        "#set($inputRoot = $input.path('$'))\n{\n"\
+                        f"  \"stateMachineArn\":\"{self._workflow.state_machine_arn}\",\n    "\
+                        "\"input\": \"{\\\"ApiRequest\\\" : "\
+                        "{\\\"Query\\\": $util.escapeJavaScript($input.json('$.Query')),"\
+                        "\\\"CallbackUrl\\\": "\
+                        "$util.escapeJavaScript($input.json('$.CallbackUrl'))}}\"\n}"
+                },
+            ),
+        )
+
+        # Create POST method
+        self._invocation_res.add_method(
+            'POST',
+            integration=self._stepfunction_integ,
+            authorization_type=_apigw.AuthorizationType.IAM,
+        )
+
         CfnOutput(self, 'StepFunctionArn',
             value = self._workflow.state_machine_arn,
             description = 'StepFunction Workflow Arn'
         )
 
-        CfnOutput(self, 'HttpApiEndpoint', 
-            description='HTTP API Endpoint', 
-            value=self._http_api.attr_api_endpoint
-        )
-
-    def _create_apigw_v2(self):
-        return _apigwv2.CfnApi(
-            self,'AsyncQueryAthenaHttpApi',
-            cors_configuration=_apigwv2.CfnApi.CorsProperty(
-                allow_credentials=False,
-                allow_headers=["*"],
-                allow_methods=['GET', 'POST', 'PUT', 'DELETE'],
-                allow_origins=["*"],
-                max_age=0,
-            ),
-            name='AsyncQueryAthenaHttpApi',
-            protocol_type='HTTP'
-        )
-
-    def _create_stage(self):
-        _stage = _apigwv2.CfnStage(self, 'HttpToSfStartExecStage',
-            api_id=self._http_api.ref,
-            stage_name='$default',
-            auto_deploy=True
-        )
-        return _stage
-
-    def _create_stepfunction_start_exec_route(self):
-        _integ =_apigwv2.CfnIntegration(
-            self, 'HttpToSfStartExecSubTypeInteg',
-            api_id=self._http_api.ref,
-            integration_type='AWS_PROXY',
-            integration_subtype='StepFunctions-StartExecution',
-            payload_format_version='1.0',
-            request_parameters={
-                'StateMachineArn': self._workflow.state_machine_arn,
-                'Input': '$request.body.Input',
-            },
-            credentials_arn=self._api_role.role_arn,
-        )
-        return _apigwv2.CfnRoute(
-            self, 'HttpApiStepfunctionStartExecRoute',
-            api_id=self._http_api.ref,
-            authorization_type='AWS_IAM',
-            route_key='POST /startexec',
-            target='/'.join(['integrations',_integ.ref]),
+        CfnOutput(self, 'ApiGatewayEndpoint', 
+            description='API Gateway Endpoint', 
+            value=self._rest_api.url
         )
 
     def _create_apigw_to_stepfunction_start_exec_role(self):
         _role = _iam.Role(
-            self, 'ApiGwHttpApiSfStartExecRole',
+            self, 'ApiGwRestApiSfStartExecRole',
             assumed_by=_iam.ServicePrincipal('apigateway.amazonaws.com')
         )
 
@@ -175,7 +163,7 @@ class AsyncQueryAthenaStack(Stack):
         _role.attach_inline_policy(
             _iam.Policy(
                 self,
-                'ApiGwHttpApiSfStartExecInlinePolicy',
+                'ApiGwRestApiSfStartExecInlinePolicy',
                 statements=[
                     _iam.PolicyStatement(
                         actions=[
@@ -187,3 +175,4 @@ class AsyncQueryAthenaStack(Stack):
             )
         )
         return _role
+
